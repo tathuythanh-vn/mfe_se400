0741d6fd05823e589977d434f82f4a73968191fa
Huy Thanh <tathuythanh.vn@gmail.com>
2025-10-25 15:52:01 +0700

    feat: add services event-registrations + favorites

--- home/src/stores/index.ts
+++ home/src/stores/index.ts
@@ -2,14 +2,29 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { authApi } from './services/auth';
 import { chapterApi } from './services/chapter';
+import { eventApi } from './services/event';
+import { commentApi } from './services/comment';
+import { favoriteApi } from './services/favorite';
+import { eventRegistrationApi } from './services/eventRegistration';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [chapterApi.reducerPath]: chapterApi.reducer,
+    [eventApi.reducerPath]: eventApi.reducer,
+    [commentApi.reducerPath]: commentApi.reducer,
+    [favoriteApi.reducerPath]: favoriteApi.reducer,
+    [eventRegistrationApi.reducerPath]: eventRegistrationApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, chapterApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      chapterApi.middleware,
+      eventApi.middleware,
+      commentApi.middleware,
+      favoriteApi.middleware,
+      eventRegistrationApi.middleware,
+    ),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -29,6 +44,43 @@ export {
 } from './services/auth';
 
 // Re-export the chapter API hooks for use in other microfrontends
-export { useGetChaptersQuery } from './services/chapter';
+export {
+  useGetChaptersInPageQuery,
+  useGetChapterStatisticQuery,
+  useGetChapterByIdQuery,
+  useCreateChapterMutation,
+  useUpdateChapterByIdMutation,
+} from './services/chapter';
+
+// Re-export the event API hooks for use in other microfrontends
+export {
+  useGetEventsInPageQuery,
+  useGetEventStatisticQuery,
+  useGetEventByIdQuery,
+  useCreateEventMutation,
+  useUpdateEventByIdMutation,
+} from './services/event';
+
+// Re-export the comment API hooks for use in other microfrontends
+export {
+  useGetCommentsQuery,
+  useCreateCommentMutation,
+  useHideCommentMutation,
+} from './services/comment';
+
+// Re-export the favorite API hooks for use in other microfrontends
+export {
+  useCheckFavoriteStatusQuery,
+  useToggleFavoriteMutation,
+} from './services/favorite';
+
+// Re-export the event registration API hooks for use in other microfrontends
+export {
+  useListEventRegistrationsQuery,
+  useRegisterForEventMutation,
+  useGetMyEventsQuery,
+  useCheckInToEventMutation,
+  useCancelEventRegistrationMutation,
+} from './services/eventRegistration';
 
 export default store;

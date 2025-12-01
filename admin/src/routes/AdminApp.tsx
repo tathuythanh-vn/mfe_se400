3608570d399b627fd7a27b50966045cb028cffb9
tranphuongVy <22521708@gm.uit.edu.vn>
2025-12-01 21:12:18 +0700

    AdminStastitic

--- admin/src/routes/AdminApp.tsx
+++ admin/src/routes/AdminApp.tsx
@@ -1,9 +1,77 @@
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Routes,
+//   Navigate,
+// } from "react-router-dom";
+
+// import MainLayout from "home/MainLayout";
+
+// import AdminAccounts from "../pages/AdminAccounts";
+// import AdminChapters from "../pages/AdminChapters";
+// import RequestAccount from "../pages/RequestAccount";
+
+// // Thêm Provider + store
+// import { Provider } from "react-redux";
+// import { store } from "../../../home/src/stores";
+
+// // Các route Admin
+// const adminRoutes = [
+//   { path: "/", element: <Navigate to="accounts" replace /> },
+//   { path: "accounts", element: <AdminAccounts /> },
+//   { path: "chapters", element: <AdminChapters /> },
+//   { path: "request-accounts", element: <RequestAccount /> },
+// ];
+
+// // Router standalone
+// const standaloneRouter = createBrowserRouter(
+//   [
+//     {
+//       path: "/admin/*",
+//       element: (
+//         <Provider store={store}>
+//           <div className="admin-override">
+//               <Routes>
+//                 {adminRoutes.map((r) => (
+//                   <Route key={r.path} path={r.path} element={r.element} />
+//                 ))}
+//               </Routes>
+//           </div>
+//         </Provider>
+//       ),
+//     },
+//   ],
+// );
+
+// type AdminAppProps = { standalone?: boolean };
+
+// export default function AdminApp({ standalone = false }: AdminAppProps) {
+//   if (standalone) {
+//     return <RouterProvider router={standaloneRouter} />;
+//   }
+
+//   // Nhúng trong Home -> KHÔNG wrap Provider (Home đã wrap rồi)
+//   return (
+//     <div className="admin-scope">
+//     <MainLayout>
+//       <Routes>
+//         {adminRoutes.map((r) => (
+//           <Route key={r.path} path={r.path} element={r.element} />
+//         ))}
+//       </Routes>
+//     </MainLayout>
+//     </div>
+//   );
+// }
+
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Routes,
   Navigate,
+  BrowserRouter,
 } from "react-router-dom";
 
 import MainLayout from "home/MainLayout";
@@ -11,10 +79,13 @@ import MainLayout from "home/MainLayout";
 import AdminAccounts from "../pages/AdminAccounts";
 import AdminChapters from "../pages/AdminChapters";
 import RequestAccount from "../pages/RequestAccount";
+import AdminStatistic from "../pages/AdminStatistic";
 
 // Thêm Provider + store
 import { Provider } from "react-redux";
 import { store } from "../../../home/src/stores";
+
+import '../App.css'
 
 // Các route Admin
 const adminRoutes = [
@@ -22,48 +93,56 @@ const adminRoutes = [
   { path: "accounts", element: <AdminAccounts /> },
   { path: "chapters", element: <AdminChapters /> },
   { path: "request-accounts", element: <RequestAccount /> },
+  { path: "stastitic", element: <AdminStatistic /> },
 ];
 
 // Router standalone
-const standaloneRouter = createBrowserRouter(
-  [
-    {
-      path: "/admin/*",
-      element: (
-        <Provider store={store}>
-          <div className="admin-override">
-              <Routes>
-                {adminRoutes.map((r) => (
-                  <Route key={r.path} path={r.path} element={r.element} />
-                ))}
-              </Routes>
-          </div>
-        </Provider>
-      ),
-    },
-  ],
-);
+const standaloneRouter = createBrowserRouter([
+  {
+    path: "/admin/*",
+    element: (
+      <Provider store={store}>
+        <div className="admin-override">
+          <Routes>
+            {adminRoutes.map((r) => (
+              <Route key={r.path} path={r.path} element={r.element} />
+            ))}
+          </Routes>
+        </div>
+      </Provider>
+    ),
+  },
+]);
 
 type AdminAppProps = { standalone?: boolean };
 
 export default function AdminApp({ standalone = false }: AdminAppProps) {
   if (standalone) {
+    // Standalone admin, dùng router riêng
     return <RouterProvider router={standaloneRouter} />;
   }
 
-  // Nhúng trong Home -> KHÔNG wrap Provider (Home đã wrap rồi)
+  // Embedded admin trong Home → không thêm Router nữa
   return (
     <div className="admin-scope">
-    <MainLayout>
-      <Routes>
-        {adminRoutes.map((r) => (
-          <Route key={r.path} path={r.path} element={r.element} />
-        ))}
-      </Routes>
-    </MainLayout>
+      <Provider store={store}>
+        <MainLayout>
+          <Routes>
+            {adminRoutes.map((r) => (
+              <Route key={r.path} path={r.path} element={r.element} />
+            ))}
+          </Routes>
+        </MainLayout>
+      </Provider>
     </div>
   );
 }
+
+
+
+
+
+
 
 // import {
 //   createBrowserRouter,
